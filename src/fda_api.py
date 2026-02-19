e9afdb3dc2e320497b71fdf931ca060918018036
mhendsch <mhendsch@bu.edu>
2026-02-19 18:21:26 -0500

    Remove string literals

--- src/fda_api.py
+++ src/fda_api.py
@@ -8,17 +8,16 @@ while (search != "Q" and search != "q"):
     firstLoop = True
     while (search!= "event" and search!= "label" and search != "enforcement" and search != "Q" and search != "q"):
         if (firstLoop == False):
-            print("""Must enter "event", "label", or "enforcement" """)
+            print("""\nMust enter "event", "label", or "enforcement" """)
         search = input("\nWhat would you like to search for? (event, label, or enforcement. Q to quit): ")
         firstLoop = False
 
-    if (search == "event"):
-        r = requests.get('https://api.fda.gov/drug/event.json?limit=1')
-    elif (search == "label"):
-        r = requests.get('https://api.fda.gov/drug/label.json?limit=1')
-    elif (search == "enforcement"):
-        r = requests.get('https://api.fda.gov/drug/enforcement.json?limit=1')
-    if (search != "Q"):
+    number_searches = -1
+    while ((int(number_searches) < 1 or number_searches.isnumeric == False) and search != "Q" and search != "q"):
+        number_searches = input("\nHow many results would you like to see?: ")
+    
+    if (search != "Q" and search != "q"):
+        r = requests.get(f'https://api.fda.gov/drug/{search}.json?limit={number_searches}')
         print("Status: ", r.status_code)
         print(r.json())
 
